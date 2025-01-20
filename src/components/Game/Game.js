b2aff1625b1bfa10b07f1d63d42beb211aac7d77
Angad Sethi <angad@easyagile.com>
2025-01-21 09:57:51 +1100

    remove log

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,8 +7,6 @@ import Guess from "../Guess/Guess";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-// console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
